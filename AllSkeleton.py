5e9a74089079b87edb1c618645706e5e0fbb8252
Arman_Riahi <158811402+chevil-dev@users.noreply.github.com>
2025-03-01 23:48:21 +0330

    Add files via upload

---
+++ AllSkeleton.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = None
+
+    def allSkeleton(start, end):
+        if start > end :
+            return [None]
+
+        allTrees = []
+        for i in range(start, end + 1):
+            leftTrees = allSkeleton(start, i - 1)
+            rightTrees = allSkeleton(i + 1, end)
+
+            for left in leftTrees:
+                for right in rightTrees:
+                    currentNode = Node(i)
+                    currentNode.left = left
+                    currentNode.right = right
+                    allTrees.append(currentNode)
+
+        return allTrees
