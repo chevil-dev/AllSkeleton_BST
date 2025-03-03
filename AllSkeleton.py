72a91365ba45ee905ebed25f29e44d2ac91fc308
Arman_Riahi <158811402+chevil-dev@users.noreply.github.com>
2025-03-03 19:27:07 +0330

    Update AllSkeleton.py

--- AllSkeleton.py
+++ AllSkeleton.py
@@ -4,20 +4,20 @@ class Node:
         self.right = None
         self.data = None
 
-    def allSkeleton(start, end):
-        if start > end :
-            return [None]
+def allSkeleton(start, end):
+    if start > end :
+        return [None]
 
-        allTrees = []
-        for i in range(start, end + 1):
-            leftTrees = allSkeleton(start, i - 1)
-            rightTrees = allSkeleton(i + 1, end)
+    allTrees = []
+    for i in range(start, end + 1):
+        leftTrees = allSkeleton(start, i - 1)
+        rightTrees = allSkeleton(i + 1, end)
 
-            for left in leftTrees:
-                for right in rightTrees:
-                    currentNode = Node(i)
-                    currentNode.left = left
-                    currentNode.right = right
-                    allTrees.append(currentNode)
+        for left in leftTrees:
+            for right in rightTrees:
+                currentNode = Node(i)
+                currentNode.left = left
+                currentNode.right = right
+                allTrees.append(currentNode)
 
-        return allTrees
+    return allTrees
